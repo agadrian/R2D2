33c3f0e7b2ed6e9a76d88d8fa03d60169aefb3bf
agadrian <agadrian@iesrafaelalberti.es>
2024-01-08 21:11:15 +0100

    .

--- R2D2/src/main/kotlin/Main.kt
+++ R2D2/src/main/kotlin/Main.kt
@@ -1,12 +1,65 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var r2d2pos: List<Int>
+
+    r2d2pos = moverRobot(10, 5, -2)
+    println("x: ${r2d2pos[0]}, y: ${r2d2pos[1]}, dir: ${orientacionRobot(r2d2pos[2])}")
+
+    moverRobot(0, 0, 0)
+    println("x: ${r2d2pos[0]}, y: ${r2d2pos[1]}, dir: ${orientacionRobot(r2d2pos[2])}")
+
+    moverRobot()
+    println("x: ${r2d2pos[0]}, y: ${r2d2pos[1]}, dir: ${orientacionRobot(r2d2pos[2])}")
 }
 
 
-fun movimientos() {
-    
+fun orientacionRobot(dir: Int) = when (dir){
+    0 -> "PositiveY"
+    1 -> "NegativeX"
+    2 -> "NegativeY"
+    3 -> "PositiveX"
+    else -> ""
+}
+
+
+/**
+ * Descripcion ddfdddf
+ * @param movs List<Int> Lista de movimientos que debe realizar el robot
+ * @return
+ */
+// vararg (Argumento variable)
+fun moverRobot(vararg movs: Int): List<Int> {
+
+    var posx = 0
+    var posy = 0
+    var dir = 0 //0 -> PositiveY; 1 -> NegativeX; 2 -> NegativeY; 3 -> PositiveX
+
+    for (pasos in movs) { // cogera cada elemento de la lista movs
+        when (dir) {
+            0 -> posy += pasos
+            1 -> posx -= pasos
+            2 -> posy -= pasos
+            3 -> posx += pasos
+        }
+
+        if (dir == 3) {
+            dir = 0
+        }
+        else{
+            dir++
+        }
+
+        return listOf(posx, posy, dir)
+        // Lo mismo de otra forma
+        /**if (dir == 0) {
+            posy += pasos
+        }
+
+        else if (dir == 1){
+            posx -= pasos
+        }
+
+        else if ()... */
+    }
+
 }
